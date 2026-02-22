1178cfe2ef1b6cd277620ecdaab9ea828a8385b0
H.C.Indievarie <arwenrhea88@gmail.com>
2026-02-22 21:28:15 +0530

    Add async/await example

--- examples/async-await.js
+++ examples/async-await.js
@@ -1,12 +1,12 @@
 const myPromise = Promise.resolve("Success using async/await!");
 
 async function myFunction() {
-try {
-const result = await myPromise;
-console.log(result);
-} catch (error) {
-console.log(error);
-}
+  try {
+    const result = await myPromise;
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 myFunction();
